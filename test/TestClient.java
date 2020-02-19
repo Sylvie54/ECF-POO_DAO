cb614741a105b6025e65ecb86062504cdd529eb1
Sylvie54 <sylvie.tcht@gmail.com>
2020-02-19 17:39:28 +0100

    ajout junit

---
+++ test/TestClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static junit.framework.TestCase.assertEquals;
+import metier.classes.Client;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sylvie
+ */
+public class TestClient  {
+    
+    public TestClient() {
+    }
+    
+    
+
+    @Test
+    public final void testInstanciation() throws Exception{
+        Client client = new Client(3,"Peugeot","null part", 3000000,30);    
+        assertTrue(true) ;  
+        
+        
+    }
+    
+}
