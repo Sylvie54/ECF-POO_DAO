4bdc38223f883bbe57b09e4d1db99c5fa3ff45e1
Sylvie54 <sylvie.tcht@gmail.com>
2020-02-20 14:20:42 +0100

    ajout tests

--- test/TestClient.java
+++ test/TestClient.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 import metier.classes.Client;
 import org.junit.After;
@@ -22,17 +23,31 @@ public class TestClient  {
     
     public TestClient() {
     }
-    
-    
-
+    @BeforeClass
+    public static void avantTest () {
+        System.out.println("----------  avant test");
+    }
     @Test
-    public final void testInstanciation() throws Exception{
+    public  void testInstanciationValide() throws Exception{
         Client client = new Client(3,"Peugeot","null part", 3000000,30);    
         assertTrue(true) ;  
     }
     @Test(expected=MetiersException.class)
-    public final void testInstanciationFail() throws MetiersException {
+    public  void testInstanciationFail() throws MetiersException {
             Client client = new Client(3,"","null part", 3000000,30); 
-            assertTrue(false) ;  
     }
+        
+    @Test(expected=MetiersException.class)
+    public  void testSetRsSociete() throws MetiersException {
+            Client client = new Client(); 
+            System.out.println("TestClient.testSetRsSociete()");
+            client.setRsSociete("");
+    }
+    private ArrayList listeSetter = new ArrayList();
+    public ArrayList source() {
+        listeSetter.add(0);
+        listeSetter.add(10);
+        return  listeSetter;
+    }
+    
 }
